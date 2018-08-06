3b7a29803e241da2985845271d1d4b7ab203ba82
Shafiul Kayem <37289068+kayem96@users.noreply.github.com>
2018-08-07 00:04:53 +0600

    Create bangla-calculator.py

---
+++ bangla-calculator.py
@@ -0,0 +1,37 @@
+#বাংলা কেল্কুলেটার বানানোর চেষ্টা
+
+operation = input('''
+
+আপনি কি যোগ (+) করবেন?
+
+নাকি বিয়োগ (-) করবেন?
+
+নাকি গুন (*) বা ভাগ (/) করবেন..?
+
+কি করবেন সেটা আগে সিলেক্ট করুন!
+
+''')
+
+number1 = int(input('এবার প্রথম নাম্বারটি দিন:'))
+
+number2 = int(input('এবার দ্বিতীয় নাম্বারটি দিন:'))
+
+if operation == '+':
+
+print('{} + {} = {}'.format(number1, number2,number1 + number2))
+
+elif operation == '-':
+
+print('{} - {} = {}'.format(number1, number2,number1 - number2))
+
+elif operation == '*':
+
+print('{} * {} = {}'.format(number1, number2,number1 * number2))
+
+elif operation == '/':
+
+print('{} / {} = {}'.format(number1,number2,number1 / number2))
+
+else:
+
+print('দুঃখিত আপনি সঠিক ভাবে লিখেন নি! প্লিজ আবার চেষ্টা করুন।')
